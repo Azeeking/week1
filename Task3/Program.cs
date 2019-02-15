7329cdb7b795125e077e1e10218344364bc20eab
Azeeking <42976057+Azeeking@users.noreply.github.com>
2019-02-15 13:04:51 +0600

    Update Program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -13,14 +13,14 @@ namespace Task3
             int n = int.Parse(Console.ReadLine());
             string s = Console.ReadLine();
             string[] a = s.Split();
-            int[] b = new int[n];
-            for (int i = 0; i < n; i++)
+            int[] b = new int[n];   // объявляем новый массив 
+            for (int i = 0; i < n; i++)  // размер массива 
             {
                 b[i] = int.Parse(a[i]);
             }
             for (int i = 0; i < n; i++)
             {
-                Console.Write(a[i] + " ");
+                Console.Write(a[i] + " ");  // вывод с дубликатом чисел
                 Console.Write(a[i] + " ");
             }
             Console.ReadKey();
